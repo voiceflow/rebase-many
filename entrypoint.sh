8789e3b4f257b3bb266d1de85a6f466f4ecbab4a
Xavientois <34867186+Xavientois@users.noreply.github.com>
2021-10-20 10:33:43 -0400

    ci: add labelling to rebase action (#5)
    * ci: add labelling to rebase action
    
    * ci: add label to repo before adding to pr
    
    * ci: improve readability of rebase-many script with if statements
    
    * ci: move comment to appropriate line

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,16 +2,34 @@
 
 NEWLINE=$'\n'
 
+FAILED_LABEL="unable-auto-rebase"
+FAILED_LABEL_COLOR="1D76DB"
+FAILED_LABEL_DESCRIPTION="Pull requests that require manual action to rebase"
+
 # Configuration
 : "${COMMENT_ON_FAILURE:=false}" # Whether to comment on the pull request that fails to rebase
+: "${LABEL_ON_FAILURE:=false}" # Whether to label the pull request that fails to rebase
 : "${LOG_LINES:=1}" # Number of log lines to include in pull request comment
 
 function rebase_failure {
+  /ghcli/bin/gh pr view $1 --json labels --jq '.labels[].name' | grep "$FAILED_LABEL" >/dev/null
+  NO_LABEL=$?
+
   # Comment on PR with reason for failure
-  "${COMMENT_ON_FAILURE}" && /ghcli/bin/gh pr comment $PR_NUMBER --body \
+  if "${COMMENT_ON_FAILURE}" && (( $NO_LABEL )); then
+    /ghcli/bin/gh pr comment $1 --body \
     "Failed to automatically rebase this pull request:$NEWLINE
     $2$NEWLINE
 More details can be found in  workflow \"$GITHUB_WORKFLOW\" at https://github.com/$GITHUB_REPOSITORY/actions/runs/$GITHUB_RUN_ID"
+  fi
+
+  if "${LABEL_ON_FAILURE}"; then
+    # Ensure label exists before adding it
+    /ghcli/bin/gh api /repos/{owner}/{repo}/labels --field name="$FAILED_LABEL" --field color="$FAILED_LABEL_COLOR" --field description="$FAILED_LABEL_DESCRIPTION" || true
+
+    # Add label denoting inability to automatically rebase
+    /ghcli/bin/gh pr edit $1 --add-label "$FAILED_LABEL"
+  fi
 }
 
 for PR_NUMBER in $PR_NUMBERS
@@ -20,6 +38,9 @@ do
   then
     REASON=$( echo "$OUTPUT" | tail -n "${LOG_LINES}" )
     rebase_failure "$PR_NUMBER" "$REASON"
+  else
+    # On success, remove label that indicates past failure
+    /ghcli/bin/gh pr edit $PR_NUMBER --remove-label "$FAILED_LABEL"
   fi
 
   # Remove fork remote for subsequent rebases
